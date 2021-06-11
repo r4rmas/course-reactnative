9e20ae4be620a520813322087e9288f4288c1bfe
Raul Armas <ra@raularmas.com>
2021-06-11 02:12:28 -0500

    ...

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, Text} from 'react-native';
+import {Image, Text, StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import CoinsStack from './src/components/coins/CoinsStack';
 import FavoritesStack from './src/components/favorites/FavoritesStack';
@@ -10,62 +10,65 @@ const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer theme={BackgroundTheme}>
-      <Tabs.Navigator
-        tabBarOptions={{
-          tintColor: '#fefefe',
-          style: {
-            alignItems: 'center',
-            backgroundColor: Colors.blackPearl,
-            height: 55,
-            paddingTop: 6,
-            paddingBottom: 6,
-          },
-        }}>
-        <Tabs.Screen
-          name="Coins"
-          component={CoinsStack}
-          options={{
-            tabBarLabel: ({focused}) => (
-              <Text style={{color: focused ? Colors.picton : Colors.gray}}>
-                Coins
-              </Text>
-            ),
-            tabBarIcon: ({focused}) => (
-              <Image
-                style={{
-                  tintColor: focused ? Colors.picton : Colors.gray,
-                  width: 28,
-                  height: 28,
-                }}
-                source={require('./src/assets/bank.png')}
-              />
-            ),
-          }}
-        />
-        <Tabs.Screen
-          name="Favorites"
-          component={FavoritesStack}
-          options={{
-            tabBarLabel: ({focused}) => (
-              <Text style={{color: focused ? Colors.picton : Colors.gray}}>
-                Favorites
-              </Text>
-            ),
-            tabBarIcon: ({focused}) => (
-              <Image
-                style={{
-                  tintColor: focused ? Colors.picton : Colors.gray,
-                  width: 28,
-                  height: 28,
-                }}
-                source={require('./src/assets/star.png')}
-              />
-            ),
-          }}
-        />
-      </Tabs.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar backgroundColor={Colors.blackPearl} />
+      <NavigationContainer theme={BackgroundTheme}>
+        <Tabs.Navigator
+          tabBarOptions={{
+            tintColor: '#fefefe',
+            style: {
+              alignItems: 'center',
+              backgroundColor: Colors.blackPearl,
+              height: 55,
+              paddingTop: 6,
+              paddingBottom: 6,
+            },
+          }}>
+          <Tabs.Screen
+            name="Coins"
+            component={CoinsStack}
+            options={{
+              tabBarLabel: ({focused}) => (
+                <Text style={{color: focused ? Colors.picton : Colors.gray}}>
+                  Coins
+                </Text>
+              ),
+              tabBarIcon: ({focused}) => (
+                <Image
+                  style={{
+                    tintColor: focused ? Colors.picton : Colors.gray,
+                    width: 28,
+                    height: 28,
+                  }}
+                  source={require('./src/assets/bank.png')}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="Favorites"
+            component={FavoritesStack}
+            options={{
+              tabBarLabel: ({focused}) => (
+                <Text style={{color: focused ? Colors.picton : Colors.gray}}>
+                  Favorites
+                </Text>
+              ),
+              tabBarIcon: ({focused}) => (
+                <Image
+                  style={{
+                    tintColor: focused ? Colors.picton : Colors.gray,
+                    width: 28,
+                    height: 28,
+                  }}
+                  source={require('./src/assets/star.png')}
+                />
+              ),
+            }}
+          />
+        </Tabs.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 
